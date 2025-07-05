c46eab2911e51a25cdcb28d99fbee9b26bb83512
Eng Abdallah Hussein <107323788+ENG3body0@users.noreply.github.com>
2025-07-05 19:32:00 +0300

    Update TMA.js

--- TMA.js
+++ TMA.js
@@ -130,7 +130,9 @@ divs.forEach(async div => {
             grouped[cup].push(match);
           });
 
-          const html = Object.entries(grouped).map(([cup, list]) => {
+          const html = Object.entries(grouped).map(([cup, list]) => 
+              const now = new Date(); // ✅ ضيفه هنا
+
            const live = [], soon = [], future = [], ended = [];
 
 list.forEach(match => {
